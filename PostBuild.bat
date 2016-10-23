9d33a42d156a4db735f681ba6f2e39c1a9fb16a0
Peter Kirmeier <topeterk@freenet.de>
2016-10-23 19:12:47 +0200

    Added post build script for creation of Portable package

---
+++ PostBuild.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo PostBuild.bat START ===========
+
+set DIR_SRC=%1
+set DIR_OUTPUT=%2
+set DIR_DEST=%3
+
+rmdir /S /Q %DIR_DEST% 2>nul
+mkdir %DIR_DEST%
+
+echo Creating portable package:
+echo ^ ^ Copy files from %DIR_OUTPUT%
+FOR %%G IN (HitCounterManager.exe HitCounterManager.exe.config) DO copy %DIR_OUTPUT%\%%G %DIR_DEST%
+echo ^ ^ Copy files from %DIR_SRC%
+FOR %%G IN (img_check.png img_cross.png HitCounter.html HitCounter.template HitCounterNoDeath.html HitCounterNumeric.html HitCounterYesNo.html) DO copy %DIR_SRC%\%%G %DIR_DEST%
+
+echo PostBuild.bat END ===========
