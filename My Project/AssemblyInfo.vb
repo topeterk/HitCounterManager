2675b5ff078b2f2c460e003d8e368319b3303b19
Peter Kirmeier <topeterk@freenet.de>
2018-01-01 00:22:26 +0100

    Happy New Year 2018 Updated copyright year

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Manages a hit counter")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("HitCounterManager")>
-<Assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2017")>
+<Assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2018")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
