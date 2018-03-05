06e257d2ecc89c13bb56d548768a4658b427fdc0
Peter Kirmeier <topeterk@freenet.de>
2018-03-05 18:11:01 +0100

    Add Appearance to Settings dialog Replaced group boxes with tab control

--- Settings.Designer.vb
+++ Settings.Designer.vb
@@ -1,6 +1,6 @@
 ﻿'MIT License
 
-'Copyright(c) 2016 Peter Kirmeier
+'Copyright(c) 2016-2018 Peter Kirmeier
 
 'Permission Is hereby granted, free Of charge, to any person obtaining a copy
 'of this software And associated documentation files (the "Software"), to deal
@@ -45,7 +45,6 @@ Partial Class Settings
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Settings))
         Me.cbScReset = New System.Windows.Forms.CheckBox()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.radioHotKeyMethod_async = New System.Windows.Forms.RadioButton()
         Me.radioHotKeyMethod_sync = New System.Windows.Forms.RadioButton()
@@ -57,7 +56,6 @@ Partial Class Settings
         Me.cbScHit = New System.Windows.Forms.CheckBox()
         Me.txtInput = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.btnOutput = New System.Windows.Forms.Button()
@@ -65,43 +63,29 @@ Partial Class Settings
         Me.txtOutput = New System.Windows.Forms.TextBox()
         Me.btnInput = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.GroupBox1.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.tab_globalshortcuts = New System.Windows.Forms.TabPage()
+        Me.tab_appearance = New System.Windows.Forms.TabPage()
+        Me.tab_filepaths = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
+        Me.tab_globalshortcuts.SuspendLayout()
+        Me.tab_filepaths.SuspendLayout()
         Me.SuspendLayout()
         '
         'cbScReset
         '
         Me.cbScReset.AutoSize = True
-        Me.cbScReset.Location = New System.Drawing.Point(6, 41)
+        Me.cbScReset.Location = New System.Drawing.Point(6, 35)
         Me.cbScReset.Name = "cbScReset"
-        Me.cbScReset.Size = New System.Drawing.Size(108, 17)
+        Me.cbScReset.Size = New System.Drawing.Size(111, 17)
         Me.cbScReset.TabIndex = 0
-        Me.cbScReset.Text = "Reset currect run"
+        Me.cbScReset.Text = "Reset currect run:"
         Me.cbScReset.UseVisualStyleBackColor = True
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.Label6)
-        Me.GroupBox1.Controls.Add(Me.radioHotKeyMethod_async)
-        Me.GroupBox1.Controls.Add(Me.radioHotKeyMethod_sync)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.txtNextSplit)
-        Me.GroupBox1.Controls.Add(Me.txtHit)
-        Me.GroupBox1.Controls.Add(Me.txtReset)
-        Me.GroupBox1.Controls.Add(Me.cbScNextSplit)
-        Me.GroupBox1.Controls.Add(Me.cbScHit)
-        Me.GroupBox1.Controls.Add(Me.cbScReset)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(596, 204)
-        Me.GroupBox1.TabIndex = 1
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Global shortcuts"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(3, 131)
+        Me.Label6.Location = New System.Drawing.Point(3, 125)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(393, 13)
         Me.Label6.TabIndex = 9
@@ -112,7 +96,7 @@ Partial Class Settings
         '
         Me.radioHotKeyMethod_async.AutoSize = True
         Me.radioHotKeyMethod_async.Checked = True
-        Me.radioHotKeyMethod_async.Location = New System.Drawing.Point(6, 170)
+        Me.radioHotKeyMethod_async.Location = New System.Drawing.Point(6, 164)
         Me.radioHotKeyMethod_async.Name = "radioHotKeyMethod_async"
         Me.radioHotKeyMethod_async.Size = New System.Drawing.Size(201, 17)
         Me.radioHotKeyMethod_async.TabIndex = 8
@@ -123,7 +107,7 @@ Partial Class Settings
         'radioHotKeyMethod_sync
         '
         Me.radioHotKeyMethod_sync.AutoSize = True
-        Me.radioHotKeyMethod_sync.Location = New System.Drawing.Point(6, 147)
+        Me.radioHotKeyMethod_sync.Location = New System.Drawing.Point(6, 141)
         Me.radioHotKeyMethod_sync.Name = "radioHotKeyMethod_sync"
         Me.radioHotKeyMethod_sync.Size = New System.Drawing.Size(477, 17)
         Me.radioHotKeyMethod_sync.TabIndex = 7
@@ -134,7 +118,7 @@ Partial Class Settings
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Location = New System.Drawing.Point(6, 10)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(338, 13)
         Me.Label1.TabIndex = 6
@@ -142,30 +126,30 @@ Partial Class Settings
         '
         'txtNextSplit
         '
-        Me.txtNextSplit.Location = New System.Drawing.Point(135, 85)
+        Me.txtNextSplit.Location = New System.Drawing.Point(123, 79)
         Me.txtNextSplit.Name = "txtNextSplit"
         Me.txtNextSplit.ReadOnly = True
-        Me.txtNextSplit.Size = New System.Drawing.Size(455, 20)
+        Me.txtNextSplit.Size = New System.Drawing.Size(459, 20)
         Me.txtNextSplit.TabIndex = 5
         Me.txtNextSplit.Text = "None"
         Me.txtNextSplit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtHit
         '
-        Me.txtHit.Location = New System.Drawing.Point(135, 62)
+        Me.txtHit.Location = New System.Drawing.Point(123, 56)
         Me.txtHit.Name = "txtHit"
         Me.txtHit.ReadOnly = True
-        Me.txtHit.Size = New System.Drawing.Size(455, 20)
+        Me.txtHit.Size = New System.Drawing.Size(459, 20)
         Me.txtHit.TabIndex = 4
         Me.txtHit.Text = "None"
         Me.txtHit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtReset
         '
-        Me.txtReset.Location = New System.Drawing.Point(135, 39)
+        Me.txtReset.Location = New System.Drawing.Point(123, 33)
         Me.txtReset.Name = "txtReset"
         Me.txtReset.ReadOnly = True
-        Me.txtReset.Size = New System.Drawing.Size(455, 20)
+        Me.txtReset.Size = New System.Drawing.Size(459, 20)
         Me.txtReset.TabIndex = 3
         Me.txtReset.Text = "None"
         Me.txtReset.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
@@ -173,62 +157,45 @@ Partial Class Settings
         'cbScNextSplit
         '
         Me.cbScNextSplit.AutoSize = True
-        Me.cbScNextSplit.Location = New System.Drawing.Point(6, 87)
+        Me.cbScNextSplit.Location = New System.Drawing.Point(6, 81)
         Me.cbScNextSplit.Name = "cbScNextSplit"
-        Me.cbScNextSplit.Size = New System.Drawing.Size(69, 17)
+        Me.cbScNextSplit.Size = New System.Drawing.Size(72, 17)
         Me.cbScNextSplit.TabIndex = 2
-        Me.cbScNextSplit.Text = "Next split"
+        Me.cbScNextSplit.Text = "Next split:"
         Me.cbScNextSplit.UseVisualStyleBackColor = True
         '
         'cbScHit
         '
         Me.cbScHit.AutoSize = True
-        Me.cbScHit.Location = New System.Drawing.Point(6, 64)
+        Me.cbScHit.Location = New System.Drawing.Point(6, 58)
         Me.cbScHit.Name = "cbScHit"
-        Me.cbScHit.Size = New System.Drawing.Size(57, 17)
+        Me.cbScHit.Size = New System.Drawing.Size(60, 17)
         Me.cbScHit.TabIndex = 1
-        Me.cbScHit.Text = "Got hit"
+        Me.cbScHit.Text = "Got hit:"
         Me.cbScHit.UseVisualStyleBackColor = True
         '
         'txtInput
         '
-        Me.txtInput.Location = New System.Drawing.Point(70, 106)
+        Me.txtInput.Location = New System.Drawing.Point(70, 99)
         Me.txtInput.Name = "txtInput"
         Me.txtInput.ReadOnly = True
-        Me.txtInput.Size = New System.Drawing.Size(440, 20)
+        Me.txtInput.Size = New System.Drawing.Size(432, 20)
         Me.txtInput.TabIndex = 6
         Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 109)
+        Me.Label2.Location = New System.Drawing.Point(6, 102)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(50, 13)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Input file:"
         '
-        'GroupBox2
-        '
-        Me.GroupBox2.Controls.Add(Me.Label7)
-        Me.GroupBox2.Controls.Add(Me.Label5)
-        Me.GroupBox2.Controls.Add(Me.btnOutput)
-        Me.GroupBox2.Controls.Add(Me.Label4)
-        Me.GroupBox2.Controls.Add(Me.txtOutput)
-        Me.GroupBox2.Controls.Add(Me.btnInput)
-        Me.GroupBox2.Controls.Add(Me.Label2)
-        Me.GroupBox2.Controls.Add(Me.txtInput)
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 222)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(596, 215)
-        Me.GroupBox2.TabIndex = 8
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Filepaths"
-        '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(6, 163)
+        Me.Label7.Location = New System.Drawing.Point(6, 159)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(561, 39)
         Me.Label7.TabIndex = 14
@@ -237,7 +204,7 @@ Partial Class Settings
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 16)
+        Me.Label5.Location = New System.Drawing.Point(6, 10)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(416, 78)
         Me.Label5.TabIndex = 7
@@ -245,7 +212,7 @@ Partial Class Settings
         '
         'btnOutput
         '
-        Me.btnOutput.Location = New System.Drawing.Point(516, 130)
+        Me.btnOutput.Location = New System.Drawing.Point(508, 123)
         Me.btnOutput.Name = "btnOutput"
         Me.btnOutput.Size = New System.Drawing.Size(74, 23)
         Me.btnOutput.TabIndex = 11
@@ -255,7 +222,7 @@ Partial Class Settings
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 135)
+        Me.Label4.Location = New System.Drawing.Point(6, 128)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(58, 13)
         Me.Label4.TabIndex = 10
@@ -263,16 +230,16 @@ Partial Class Settings
         '
         'txtOutput
         '
-        Me.txtOutput.Location = New System.Drawing.Point(70, 132)
+        Me.txtOutput.Location = New System.Drawing.Point(70, 125)
         Me.txtOutput.Name = "txtOutput"
         Me.txtOutput.ReadOnly = True
-        Me.txtOutput.Size = New System.Drawing.Size(440, 20)
+        Me.txtOutput.Size = New System.Drawing.Size(432, 20)
         Me.txtOutput.TabIndex = 9
         Me.txtOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'btnInput
         '
-        Me.btnInput.Location = New System.Drawing.Point(516, 104)
+        Me.btnInput.Location = New System.Drawing.Point(508, 97)
         Me.btnInput.Name = "btnInput"
         Me.btnInput.Size = New System.Drawing.Size(74, 23)
         Me.btnInput.TabIndex = 8
@@ -283,29 +250,87 @@ Partial Class Settings
         '
         Me.OpenFileDialog1.FileName = "HitCounter*"
         '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.tab_globalshortcuts)
+        Me.TabControl1.Controls.Add(Me.tab_appearance)
+        Me.TabControl1.Controls.Add(Me.tab_filepaths)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(596, 235)
+        Me.TabControl1.TabIndex = 9
+        '
+        'tab_globalshortcuts
+        '
+        Me.tab_globalshortcuts.Controls.Add(Me.Label6)
+        Me.tab_globalshortcuts.Controls.Add(Me.radioHotKeyMethod_async)
+        Me.tab_globalshortcuts.Controls.Add(Me.Label1)
+        Me.tab_globalshortcuts.Controls.Add(Me.radioHotKeyMethod_sync)
+        Me.tab_globalshortcuts.Controls.Add(Me.cbScReset)
+        Me.tab_globalshortcuts.Controls.Add(Me.cbScHit)
+        Me.tab_globalshortcuts.Controls.Add(Me.txtNextSplit)
+        Me.tab_globalshortcuts.Controls.Add(Me.cbScNextSplit)
+        Me.tab_globalshortcuts.Controls.Add(Me.txtHit)
+        Me.tab_globalshortcuts.Controls.Add(Me.txtReset)
+        Me.tab_globalshortcuts.Location = New System.Drawing.Point(4, 22)
+        Me.tab_globalshortcuts.Name = "tab_globalshortcuts"
+        Me.tab_globalshortcuts.Padding = New System.Windows.Forms.Padding(3, 10, 3, 3)
+        Me.tab_globalshortcuts.Size = New System.Drawing.Size(588, 209)
+        Me.tab_globalshortcuts.TabIndex = 0
+        Me.tab_globalshortcuts.Text = "Global shortcuts"
+        Me.tab_globalshortcuts.UseVisualStyleBackColor = True
+        '
+        'tab_appearance
+        '
+        Me.tab_appearance.Location = New System.Drawing.Point(4, 22)
+        Me.tab_appearance.Name = "tab_appearance"
+        Me.tab_appearance.Padding = New System.Windows.Forms.Padding(3, 10, 3, 3)
+        Me.tab_appearance.Size = New System.Drawing.Size(588, 213)
+        Me.tab_appearance.TabIndex = 1
+        Me.tab_appearance.Text = "Appearance"
+        Me.tab_appearance.UseVisualStyleBackColor = True
+        '
+        'tab_filepaths
+        '
+        Me.tab_filepaths.Controls.Add(Me.Label7)
+        Me.tab_filepaths.Controls.Add(Me.Label5)
+        Me.tab_filepaths.Controls.Add(Me.btnOutput)
+        Me.tab_filepaths.Controls.Add(Me.txtInput)
+        Me.tab_filepaths.Controls.Add(Me.Label4)
+        Me.tab_filepaths.Controls.Add(Me.Label2)
+        Me.tab_filepaths.Controls.Add(Me.txtOutput)
+        Me.tab_filepaths.Controls.Add(Me.btnInput)
+        Me.tab_filepaths.Location = New System.Drawing.Point(4, 22)
+        Me.tab_filepaths.Name = "tab_filepaths"
+        Me.tab_filepaths.Padding = New System.Windows.Forms.Padding(3, 10, 3, 3)
+        Me.tab_filepaths.Size = New System.Drawing.Size(588, 209)
+        Me.tab_filepaths.TabIndex = 2
+        Me.tab_filepaths.Text = "Filepaths"
+        Me.tab_filepaths.UseVisualStyleBackColor = True
+        '
         'Settings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(620, 449)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.ClientSize = New System.Drawing.Size(620, 259)
+        Me.Controls.Add(Me.TabControl1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Settings"
         Me.Text = "Settings"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        Me.GroupBox2.ResumeLayout(False)
-        Me.GroupBox2.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.tab_globalshortcuts.ResumeLayout(False)
+        Me.tab_globalshortcuts.PerformLayout()
+        Me.tab_filepaths.ResumeLayout(False)
+        Me.tab_filepaths.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents cbScReset As CheckBox
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents cbScHit As CheckBox
     Friend WithEvents cbScNextSplit As CheckBox
     Friend WithEvents txtNextSplit As TextBox
@@ -314,7 +339,6 @@ Partial Class Settings
     Friend WithEvents Label1 As Label
     Friend WithEvents txtInput As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents btnInput As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents btnOutput As Button
@@ -325,4 +349,8 @@ Partial Class Settings
     Friend WithEvents radioHotKeyMethod_async As RadioButton
     Friend WithEvents radioHotKeyMethod_sync As RadioButton
     Friend WithEvents Label7 As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents tab_globalshortcuts As TabPage
+    Friend WithEvents tab_appearance As TabPage
+    Friend WithEvents tab_filepaths As TabPage
 End Class
