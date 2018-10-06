49491e445c5ebe16eb6c2f49997415a6c1e4fb24
Peter Kirmeier <topeterk@freenet.de>
2018-10-07 00:09:25 +0200

    Add license text

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,5 +1,27 @@
 @echo off
 
+REM MIT License
+
+REM Copyright(c) 2016-2018 Peter Kirmeier
+
+REM Permission Is hereby granted, free Of charge, to any person obtaining a copy
+REM of this software And associated documentation files (the "Software"), to deal
+REM in the Software without restriction, including without limitation the rights
+REM to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+REM copies of the Software, And to permit persons to whom the Software Is
+REM furnished to do so, subject to the following conditions:
+
+REM The above copyright notice And this permission notice shall be included In all
+REM copies Or substantial portions of the Software.
+
+REM THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+REM IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+REM FITNESS FOR A PARTICULAR PURPOSE And NONINFRINGEMENT. IN NO EVENT SHALL THE
+REM AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+REM LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+REM OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE
+REM SOFTWARE.
+
 echo PostBuild.bat START ===========
 
 set DIR_SRC=%1
