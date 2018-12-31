4ccc24e881d6325a973b6c8758b870a71d324e49
Peter Kirmeier <topeterk@freenet.de>
2019-01-01 00:00:44 +0100

    Happy New Year Prepared application's copyright notes for year 2019

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HitCounterManager")]
-[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2018")]
+[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
