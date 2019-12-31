d19a84711ceeff26304c632346dabb16df7c7e63
Peter Kirmeier <topeterk@users.noreply.github.com>
2020-01-01 00:05:32 +0100

    Happy New Year! Updated copyright year

--- Sources/AssemblyInfo.cs
+++ Sources/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HitCounterManager")]
-[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2019")]
+[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
