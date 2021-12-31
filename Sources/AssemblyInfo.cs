5fb033426fc78e34d129702a0c32b8c46df250fb
Peter Kirmeier <topeterk@users.noreply.github.com>
2022-01-01 00:27:52 +0100

    Happy New Year! Updated copyright year to 2022

--- Sources/AssemblyInfo.cs
+++ Sources/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HitCounterManager")]
-[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2021")]
+[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
