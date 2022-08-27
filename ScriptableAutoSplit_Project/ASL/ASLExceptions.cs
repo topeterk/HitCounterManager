4dad87f698947f28c91ea79d338cb184cc5c29c5
Neimex23 <ezequielmedina23@gmail.com>
2022-08-27 15:24:05 -0300

    Folders Changes

---
+++ ScriptableAutoSplit_Project/ASL/ASLExceptions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace LiveSplit.ASL
+{
+    public class ASLCompilerException : Exception
+    {
+        public ASLMethod Method { get; }
+
+        public CompilerErrorCollection CompilerErrors { get; }
+
+        public ASLCompilerException(ASLMethod method, CompilerErrorCollection errors)
+            : base(GetMessage(method, errors))
+        {
+            Method = method;
+            CompilerErrors = errors;
+        }
+
+        static string GetMessage(ASLMethod method, CompilerErrorCollection errors)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+            if (errors == null)
+                throw new ArgumentNullException(nameof(errors));
+
+            var sb = new StringBuilder($"'{method.Name ?? "(no name)"}' method compilation errors:");
+            foreach (CompilerError error in errors)
+            {
+                error.Line = error.Line + method.LineOffset;
+                sb.Append($"\nLine {error.Line}, Col {error.Column}: {(error.IsWarning ? "warning" : "error")} {error.ErrorNumber}: {error.ErrorText}");
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class ASLRuntimeException : Exception
+    {
+        public ASLRuntimeException(ASLMethod method, Exception inner_exception)
+            : base(GetMessage(method, inner_exception), inner_exception)
+        { }
+
+        static string GetMessage(ASLMethod method, Exception inner_exception)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+            if (inner_exception == null)
+                throw new ArgumentNullException(nameof(inner_exception));
+
+            var stack_trace = new StackTrace(inner_exception, true);
+            var stack_trace_sb = new StringBuilder();
+            foreach (var frame in stack_trace.GetFrames())
+            {
+                var frame_method = frame.GetMethod();
+                var frame_module = frame_method.Module;
+
+                var frame_asl_method = method;
+                if (method.ScriptMethods != null)
+                {
+                    frame_asl_method = method.ScriptMethods.FirstOrDefault(m => frame_module == m.Module);
+                    if (frame_asl_method == null)
+                        continue;
+                }
+                else if (frame_module != method.Module)
+                    continue;
+
+                var frame_line = frame.GetFileLineNumber();
+                if (frame_line > 0)
+                {
+                    var line = frame_line + frame_asl_method.LineOffset;
+                    stack_trace_sb.Append($"\n   at ASL line {line} in '{frame_asl_method.Name}'");
+                }
+            }
+
+            var exception_name = inner_exception.GetType().FullName;
+            var method_name = method.Name ?? "(no name)";
+            var exception_message = inner_exception.Message;
+            return $"Exception thrown: '{exception_name}' in '{method_name}' method:\n{exception_message}\n{stack_trace_sb.ToString()}";
+        }
+    }
+}
---
+++ ScriptableAutoSplit_Project/ASL/ASLState.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using LiveSplit.ComponentUtil;
+
+namespace LiveSplit.ASL
+{
+    public class ASLValueDefinition
+    {
+        public string Type { get; set; }
+        public string Identifier { get; set; }
+        public DeepPointer Pointer { get; set; }
+    }
+
+    public class ASLState : ICloneable
+    {
+        public ExpandoObject Data { get; set; }
+        public List<ASLValueDefinition> ValueDefinitions { get; set; }
+        public string GameVersion { get; set; }
+
+        public ASLState()
+        {
+            Data = new ExpandoObject();
+            ValueDefinitions = new List<ASLValueDefinition>();
+            GameVersion = string.Empty;
+        }
+
+        public ASLState RefreshValues(Process p)
+        {
+            var clone = (ASLState)Clone();
+            var dict = ((IDictionary<string, object>)Data);
+
+            foreach (var value_definition in ValueDefinitions)
+            {
+                var value = GetValue(p, value_definition.Type, value_definition.Pointer);
+
+                if (dict.ContainsKey(value_definition.Identifier))
+                    dict[value_definition.Identifier] = value;
+                else
+                    dict.Add(value_definition.Identifier, value);
+            }
+
+            return clone;
+        }
+
+        private static dynamic GetValue(Process p, string type, DeepPointer pointer)
+        {
+            switch (type)
+            {
+                case "int":
+                    return pointer.Deref<int>(p);
+                case "uint":
+                    return pointer.Deref<uint>(p);
+                case "long":
+                    return pointer.Deref<long>(p);
+                case "ulong":
+                    return pointer.Deref<ulong>(p);
+                case "float":
+                    return pointer.Deref<float>(p);
+                case "double":
+                    return pointer.Deref<double>(p);
+                case "byte":
+                    return pointer.Deref<byte>(p);
+                case "sbyte":
+                    return pointer.Deref<sbyte>(p);
+                case "short":
+                    return pointer.Deref<short>(p);
+                case "ushort":
+                    return pointer.Deref<ushort>(p);
+                case "bool":
+                    return pointer.Deref<bool>(p);
+                default:
+                    if (type.StartsWith("string"))
+                    {
+                        var length = int.Parse(type.Substring("string".Length));
+                        return pointer.DerefString(p, length);
+                    }
+                    else if (type.StartsWith("byte"))
+                    {
+                        var length = int.Parse(type.Substring("byte".Length));
+                        return pointer.DerefBytes(p, length);
+                    }
+                    break;
+            }
+
+            throw new ArgumentException($"The provided type, '{type}', is not supported");
+        }
+
+        public object Clone()
+        {
+            var clone = new ExpandoObject();
+            foreach (var pair in Data)
+            {
+                ((IDictionary<string, object>)clone).Add(pair);
+            }
+            return new ASLState() { Data = clone, ValueDefinitions = new List<ASLValueDefinition>(ValueDefinitions) };
+        }
+    }
+}
