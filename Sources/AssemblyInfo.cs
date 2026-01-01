2a374a66b82a6caaf7fe3d0d091263719d95ae13
Peter Kirmeier <topeterk@users.noreply.github.com>
2026-01-01 01:50:34 +0100

    Happy New Year!
    Updated copyright year of license.

--- Sources/AssemblyInfo.cs
+++ Sources/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿//MIT License
 
-//Copyright (c) 2016-2025 Peter Kirmeier
+//Copyright (c) 2016-2026 Peter Kirmeier
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HitCounterManager")]
-[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2025")]
+[assembly: AssemblyCopyright("Copyright © Peter Kirmeier 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
